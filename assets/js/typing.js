5be3bbdca77e61fc95d03230954f35a0d02e6904
troyshopping <154373123+troyshopping@users.noreply.github.com>
2024-05-10 16:46:35 -0700

    Add files via upload

---
+++ assets/js/typing.js
@@ -0,0 +1,42 @@
+const sentences = [
+    "Yeo",
+    "Fartsec",
+    "1337 hacker!",
+    "discord.gg/ruinus",
+    "Ws is full of pedos",
+    "Chilli - percaso",
+    "Red mist",
+    "@Fartsec"
+];
+
+const typewriter = document.getElementById('bio');
+let sentenceIndex = 0;
+let letterIndex = 0;
+let isDeleting = false;
+
+function type() {
+    const currentSentence = sentences[sentenceIndex];
+    if (isDeleting) {
+        typewriter.textContent = currentSentence.substring(0, letterIndex - 1);
+        letterIndex--;
+    } else {
+        typewriter.textContent = currentSentence.substring(0, letterIndex + 1);
+        letterIndex++;
+    }
+
+    const typingSpeed = 100; 
+    let typingDelay = isDeleting ? 50 : typingSpeed;
+
+    if (!isDeleting && letterIndex === currentSentence.length) {
+        typingDelay = 1500; 
+        isDeleting = true;
+    } else if (isDeleting && letterIndex === 0) {
+        isDeleting = false;
+        sentenceIndex = (sentenceIndex + 1) % sentences.length;
+        typingDelay = 500; 
+    }
+
+    setTimeout(type, typingDelay);
+}
+
+setTimeout(type, 1000); 
