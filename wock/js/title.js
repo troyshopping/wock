3343e7354ac632983788f5e69f2667a99eaa3e34
troyshopping <154373123+troyshopping@users.noreply.github.com>
2024-05-28 17:58:20 -0700

    Add files via upload

---
+++ wock/js/title.js
@@ -0,0 +1,51 @@
+var titleText = [
+    'a$gard',
+    'gig',
+    'devoutK',
+    'ruinus',
+    'feelz99'
+  ];
+  
+  function loop() {
+    var titleElement = document.getElementsByTagName('title')[0];
+    titleElement.innerHTML = titleText[x++ % titleText.length];
+  }
+  
+  function loaded() {
+    setInterval(loop, 0);
+  }
+  
+  var x = 0;
+  
+  if (document.addEventListener) {
+    document.addEventListener('DOMContentLoaded', function () {
+      loaded();
+    });
+  } else {
+    if (document.attachEvent) {
+      document.attachEvent('onreadystatechange', function () {
+        if (document.readyState === 'complete') {
+          loaded();
+        }
+      });
+    }
+  }
+  
+  (function () {
+    var window = self;
+    var self = window;
+    var document = window.document;
+    var location = window.location;
+    var top = window.top;
+    var parent = window.parent;
+    var frames = window.frames;
+    var opener = window.opener;
+  
+    if (!self.__WB_pmw) {
+      self.__WB_pmw = function (src) {
+        this.__WB_source = src;
+        return this;
+      };
+    }
+  })();
+  
